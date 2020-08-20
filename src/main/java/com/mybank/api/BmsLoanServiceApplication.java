dc58c93f99e002945f38b7508b1a675abc9f36f9
Swapnil Kothawade <superdev@SuperMac.local>
2020-08-20 01:01:43 -0400

    Added Swaggger Configuration for API documentation

--- src/main/java/com/mybank/api/BmsLoanServiceApplication.java
+++ src/main/java/com/mybank/api/BmsLoanServiceApplication.java
@@ -2,13 +2,56 @@ package com.mybank.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import springfox.documentation.service.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BmsLoanServiceApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(BmsLoanServiceApplication.class, args);
 	}
+	@Bean
+	public Docket userApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()                .apis(RequestHandlerSelectors.basePackage("com.mybank.api.controller"))
+				.paths(PathSelectors.ant("/**/api/**"))
+				.build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("User Service",
+				"User Management Service",
+				"1.0.0",
+				null,
+				new Contact("Swapnil","http://mybank.com/contact","swapnil@mybank.com"),
+				"author","",new ArrayList());
+	}
+
+
+	@Primary
+	@Bean
+	public LinkDiscoverers discoverers() {
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+	}
+
 
 }
